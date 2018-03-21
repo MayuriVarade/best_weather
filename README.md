85cd7df5bdab211b2753c9880961ee9ff130ef5c
MayuriVarade <mayurisupe@gmail.com>
2018-03-21 14:19:56 +0100

    compared weather logic

--- README.md
+++ README.md
@@ -1,43 +1,4 @@
 
-XING Ruby on Rails Coding Challenge
-
-
-Welcome to the coding challenge.
-
-Welcome to the XING Coding Challenge! This challenge will give us an idea about your coding skills. You get access to this repository for 24 hours. Please plan around 3 hours for this challenge.
-
-Prerequisites
---------------
-
-- Important for us is code efficiency, following of best practices & code readability
-- Please don't use scaffolding
-- Do not focus too much on the design
-- The usage of third party libraries is explicitly allowed
-- Provide a comprehensive git history
-- Commit directly to master and do not open a PR, then we can efficiently transfer the repo back to our GitHub Enterprise
-
-Goals
------
-
-- Please create a very simple web application in Ruby on Rails 4 or 5. 
-- The application should compare the current weather of 5 cities of your choice (fine to hard-wire the cities) and makes a recommendation, which of the cities currently has the best weather. 
-- Feel free to add a readme that describes how your app works and what things you would add/change/do differently if you were given more time. 
-
-Bonus Goals
-------------
-
-- Provide a basic set of meaningful tests
-- Add a UI for users to set the compared cities (2-5)
-
-Hints
-------
-
-- OpenWeatherMap has an API to get the current weather: https://openweathermap.org/current
-
---------------------
-
-Solution:
--------------------
 1. Ruby Version:2.3.3
 2. Rails Version: 5.1.5
 3. Open Weather API Key (http://home.openweathermap.org/users/sign_up)
@@ -48,9 +9,6 @@ Setup Locally
 -----------
 Clone Repo:
 (https://github.com/colinxchristmas/forecaster.git)
-
-Change dir into test_ruby_weather-MayuriVarade:
-- cd test_ruby_weather-MayuriVarade
 
 Install Gems:
 - bundle install
--- app/controllers/concerns/weather.rb
+++ app/controllers/concerns/weather.rb
@@ -16,4 +16,43 @@ module Weather
     temp - 273.15
   end 
 
+def compare
+all_temperature = [get_city_1["temp"],get_city_2["temp"],get_city_3["temp"],get_city_4["temp"],get_city_5["temp"]].sort!
+    max_temp = all_temperature.last
+    min_temp = all_temperature.first
+    @avg_temp = (max_temp + min_temp)/2
+    r1 = (@avg_temp-5)
+    r2 = (@avg_temp+5)
+
+    data1 = []
+
+    data1 << {"city" => get_city_1["city"], "temp" => get_city_1["temp"]} if (get_weather_1 == "Clear") && get_city_1["temp"].between?(r1,r2)
+    data1 << {"city" => get_city_2["city"], "temp" => get_city_2["temp"]} if (get_weather_2 == "Clear") && get_city_2["temp"].between?(r1,r2)
+    data1 << {"city" => get_city_3["city"], "temp" => get_city_3["temp"]} if (get_weather_3 == "Clear") && get_city_3["temp"].between?(r1,r2)
+    data1 << {"city" => get_city_4["city"], "temp" => get_city_4["temp"]} if (get_weather_4 == "Clear") && get_city_4["temp"].between?(r1,r2)
+    data1 << {"city" => get_city_5["city"], "temp" => get_city_5["temp"]} if (get_weather_5 == "Clear") && get_city_5["temp"].between?(r1,r2)   
+
+    data2 = []
+
+    data2 << {"city" => get_city_1["city"], "temp" => get_city_1["temp"]} if (get_weather_1 == "Clouds") && get_city_1["temp"].between?(r1,r2)
+    data2 << {"city" => get_city_2["city"], "temp" => get_city_2["temp"]} if (get_weather_2 == "Clouds") && get_city_2["temp"].between?(r1,r2)
+    data2 << {"city" => get_city_3["city"], "temp" => get_city_3["temp"]} if (get_weather_3 == "Clouds") && get_city_3["temp"].between?(r1,r2)
+    data2 << {"city" => get_city_4["city"], "temp" => get_city_4["temp"]} if (get_weather_4 == "Clouds") && get_city_4["temp"].between?(r1,r2)
+    data2 << {"city" => get_city_5["city"], "temp" => get_city_5["temp"]} if (get_weather_5 == "Clouds") && get_city_5["temp"].between?(r1,r2)    
+
+    @recommended_city = ''
+
+    if data1.empty?
+      arr1 = []
+      final_weather = data2.max_by { |x| x["temp"].to_f}
+      @recommended_city = final_weather["city"]
+    else
+      arr2 = []
+      final_weather2 = data1.max_by { |x| x["temp"].to_f}
+      @recommended_city = final_weather2["city"]  
+    end 
+    @weather_all_cities = [get_city_1, get_city_2, get_city_3, get_city_4, get_city_5]
+
+  
+end
 end
